03c40ff471df31591acfa586569f803137276659
chloele33 <chloele@seas.upenn.edu>
2017-11-25 17:33:09 -0500

    all schemas added

--- db/user.js
+++ db/user.js
@@ -6,15 +6,43 @@ var Schema = mongoose.Schema;
 // bcrypt for securing password information
 var bcrypt = require('bcrypt');
 
+// define schemas
 var userSchema = new Schema({
   username: { type: String, lowercase: true, required: true, unique: true },
   password: { type: String, required: true },
   name: { type: String, required: true},
   email: {type: String, lowercase: true, required: true, unique: true, match: [/\S+@\S+\.\S+/, 'Invalid Email. Valid email example: hello@foodiegram.com']},
   bio: { type: String},
-
+  posts: [{type: Schema.ObjectId, ref: 'Post'}],
+  following: [{ type: Schema.ObjectId, ref: 'User' }],
+  followers: [{ type: Schema.ObjectId, ref: 'User' }],
+  favorites: [{type: Schema.ObjectId, ref: 'Post'}]
 });
 
+var postSchema = new Schema({
+  _creater: { type: Schema.ObjectId, ref: 'User' }, 
+  likes: [{ type: Schema.ObjectId, ref: 'User' }], 
+  rating: {type: Number, max: 5, min: 0},
+  caption: { type: String },
+  created_at: Date, 
+  updated_at: Date,
+  place: {type: Schema.ObjectId, ref: 'Place'},
+  comments: [{ type: Schema.ObjectId, ref: 'Comment' }]
+});
+
+var placeSchema = new Schema({
+  name: {type: String},
+  latitude :{ type: Number}, 
+  longtitude: { type: Number}
+});
+
+var commentSchema = new Schema({
+  content: {type: String},
+  author : { type: Schema.ObjectId, ref: 'User' }, 
+  created_at: Date
+});
+
+// user schema functions
 userSchema.pre('save', function(next) {
   var user = this;
 
@@ -30,7 +58,6 @@ userSchema.pre('save', function(next) {
     });
   });
 });
-
 // bad practice, becasue actually should check if username exists in the database. 
 // this also throws an error, but we should throw an error intentionally ourselves. 
 userSchema.statics.addUser = function(username, password, name, email, cb) {
@@ -50,6 +77,11 @@ userSchema.statics.checkIfLegit = function(username, password, cb) {
   });
 }
 
+// save models
 var User = mongoose.model('User', userSchema);
+var Place = mongoose.model('Place', placeSchema);
+var Post = mongoose.model('Post', postSchema);
+var Comment = mongoose.model('Comment', commentSchema);
+
 
 module.exports = User;
